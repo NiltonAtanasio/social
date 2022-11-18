0049ced80ac858c0dca59dd407fb5cf4595d7a72
Nilton Atanazzio <nii.cesar@hotmail.com>
2022-11-18 18:45:07 -0300

    😎 Rota com autenticacao e paginação de dados

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,8 +1,8 @@
-import { createService, findAllService } from '../service/news.service.js'
-import { ObjectId } from 'mongoose'
+import { createService, findAllService, countNews} from '../service/news.service.js'
 
 const create = async ( req, res ) => {
   try {
+    
     const { title, text, banner } = req.body
     
     if(!title || !text || !banner){
@@ -15,7 +15,7 @@ const create = async ( req, res ) => {
       title,
       text,
       banner,
-      user: {_id: "6368656382e68839af256291"}
+      user: req.userId
     })
 
     res.status(201)
@@ -25,14 +25,55 @@ const create = async ( req, res ) => {
 }
 
 const findAll = async ( req, res ) => {
-  const news = await findAllService()
+  let { limit, offset } = req.query
+
+  limit = Number(limit)
+  offset = Number(offset)
+
+  if(!limit){
+    limit = 5
+  }
+
+  if(!offset){
+    offset - 0
+  }
+
+  const news = await findAllService(offset, limit)
+  const total = await countNews()
+  const currentUrl = req.baseUrl
+
+  const next = offset + limit
+  const nextUrl = 
+    next < total ? `${currentUrl}?limit=${limit}$offset=${next}` : null
+   
+  const previous = offset - limit < 0 ? null : offset - limit
+  const previousUrl =  
+    previous != null ? `${currentUrl}?limit=${limit}$offset=$previous}` : null
 
   if(news.length === 0){
     return res.status(400).send({
       message: "There are no registered news"
     })
   }
-  res.send(news)
+  res.send({
+    nextUrl,
+    previousUrl,
+    limit,
+    offset,
+    total,
+
+    results: news.map(item => ({
+      id: item._id,
+      title: item.title,
+      text: item.text,
+      banner: item.banner,
+      likes: item.likes,
+      comments: item.comments,
+      name: item.user.name,
+      userName: item.user.username,
+      userAvatar: item.user.avatar
+    }))
+  })
 }
 
 export { create, findAll }
