205ca5453598206c790863df573d6137a8d9b97a
Nilton Atanazzio <nii.cesar@hotmail.com>
2022-11-21 21:30:54 -0300

    🥸 Search by title

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService, countNews, topNewsService, findByIdService } from '../service/news.service.js'
+import { createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService } from '../service/news.service.js'
 
 const create = async (req, res) => {
   try {
@@ -133,4 +133,35 @@ const findById = async (req, res) => {
     res.status(500).send({ message: error.message })
   }
 }
-export { create, findAll, topNews, findById }
+
+const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query
+
+    const news = await searchByTitleService(title)
+    
+    if(news.length === 0){
+      return res.status(400).send({
+        message: "There are no news with this title"
+      })
+    }
+
+    return res.send({
+      results: news.map(item => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        userName: item.user.username,
+        userAvatar: item.user.avatar
+      }))
+    })
+
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+}
+export { create, findAll, topNews, findById, searchByTitle }
